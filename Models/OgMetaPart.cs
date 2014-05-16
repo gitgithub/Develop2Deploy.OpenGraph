66ca75427b4b607d517b750ac48403891c771c24
abhishekluv <abhishek.luv@outlook.com>
2014-05-17 00:03:25 +0530

    Initial Commit

---
+++ Models/OgMetaPart.cs
@@ -0,0 +1,45 @@
+﻿using Orchard.ContentManagement;
+using Orchard.Environment.Extensions;
+
+namespace Develop2Deploy.OpenGraph.Models
+{
+    [OrchardFeature("Develop2Deploy.OpenGraph")]
+    public class OgMetaPart : ContentPart<OgMetaRecord>
+    {
+        public string OgTitle
+        {
+            get { return Retrieve(t => t.OgTitle); }
+            set { Store(t => t.OgTitle, value); }
+        }
+
+        public string OgType
+        {
+            get { return Retrieve(t => t.OgType); }
+            set { Store(t => t.OgType, value); }
+        }
+
+        public string OgUrl
+        {
+            get { return Retrieve(u => u.OgUrl); }
+            set { Store(u => u.OgUrl, value); }
+        }
+
+        public string OgImgUrl
+        {
+            get { return Retrieve(u => u.OgImgUrl); }
+            set { Store(u => u.OgImgUrl, value); }
+        }
+
+        public string OgDescription
+        {
+            get { return Retrieve(d => d.OgDescription); }
+            set { Store(d => d.OgDescription, value); }
+        }
+
+        public string OgSiteName
+        {
+            get { return Retrieve(s => s.OgSiteName); }
+            set { Store(s => s.OgSiteName, value); }
+        }
+    }
+}
